f0f88b840c0fe2e1310e808eb3600a9d51a93e28
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-24 20:21:23 -0400

    initial server

--- server.js
+++ server.js
@@ -2,14 +2,37 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const sess = {
+  secret: 'SecretShhh',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// turn on routes
-app.use(routes);
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({helpers});
+
+app.engine('handlebars', hbs.engine);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
